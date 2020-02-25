bb5571700b2132e8296651896cfd9d7b39a5e4fa
op07n <39182105+op07n@users.noreply.github.com>
2020-02-25 11:47:57 +0100

    Update test_package.cpp

--- test_package.cpp
+++ test_package.cpp
@@ -1,49 +1,40 @@
-/*
-    Copyright (c) 2017-2018 Intel Corporation
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-        http://www.apache.org/licenses/LICENSE-2.0
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-
-/*
-    This file contains the implementation of dot product based on std::transform_reduce
-*/
-
-#include <vector>
-#include <random>
+#include "tbb/task_group.h"  
+#include "tbb/flow_graph.h"
+#include "tbb/compat/tuple"
 #include <iostream>
-#include <pstl/algorithm>
-#include <pstl/numeric>
-#include <pstl/execution>
 
-double random_number_generator() {
-    // usage of thread local random engines allows running the generator in concurrent mode
-    thread_local static std::default_random_engine rd;
-    std::uniform_real_distribution<double> dist(0, 1);
-    return dist(rd);
+using namespace tbb;
+using namespace tbb::flow;
+
+int Fib(int n) {
+    if( n<2 ) {
+        return n;
+    } else {
+        int x, y;
+        task_group g;
+        g.run([&]{x=Fib(n-1);}); // spawn a task
+        g.run([&]{y=Fib(n-2);}); // spawn another task
+        g.wait();                // wait for both tasks to complete
+        return x+y;
+    }
 }
 
-int main(int argc, char* argv[]) {
+int main(){
+    std::cout<<"Fib 6="<<Fib(6)<<"\n";
 
-    const size_t size = 10000000;
-
-    std::vector<double> v1(size), v2(size);
-
-    //initialize vectors with random numbers
-    std::generate(pstl::execution::par, v1.begin(), v1.end(), random_number_generator);
-    std::generate(pstl::execution::par, v2.begin(), v2.end(), random_number_generator);
-
-    //the dot product calculation
-    double res = std::transform_reduce(pstl::execution::par_unseq, v1.cbegin(), v1.cend(), v2.cbegin(), .0,
-        std::plus<double>(), std::multiplies<double>());
-
-    std::cout << "The dot product is: " << res << std::endl;
-
+    graph g;
+    continue_node< continue_msg> hello( g,
+      []( const continue_msg &) {
+          std::cout << "Hello";
+      }
+    );
+    continue_node< continue_msg> world( g,
+      []( const continue_msg &) {
+          std::cout << " World\n";
+      }
+    );
+    make_edge(hello, world);
+    hello.try_put(continue_msg());
+    g.wait_for_all();
     return 0;
 }
